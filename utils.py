8437c01132bb526fd60b665785ecfddd2c49c03b
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2020-10-14 14:34:45 -0700

    Preliminary analysis of a video

---
+++ utils.py
@@ -0,0 +1,137 @@
+import os
+import json
+import numpy as np
+
+FPS = 30.0
+
+NOSE = 0
+NECK = 1
+RSHO = 2
+RELB = 3
+RWRI = 4
+LSHO = 5
+LELB = 6
+LWRI = 7
+MHIP = 8
+RHIP = 9
+RKNE = 10
+RANK = 11
+LHIP = 12
+LKNE = 13
+LANK = 14
+REYE = 15
+LEYE = 16
+REAR = 17
+LEAR = 18
+LBTO = 19
+LSTO = 20
+LHEL = 21
+RBTO = 22
+RSTO = 23
+RHEL = 24
+
+# Convert OpenPose frames to a numpy array
+def json2np(json_dir):
+    n = len(os.listdir(json_dir))
+    res = np.zeros((n,75))
+    for frame in range(n):
+        test_image_json = '{}/input_{}_keypoints.json'.format(json_dir, str(frame).zfill(12))
+
+        with open(test_image_json) as data_file:  
+            data = json.load(data_file)
+
+        for person in data['people']:
+            keypoints = person['pose_keypoints_2d']
+            xcoords = [keypoints[i] for i in range(len(keypoints)) if i % 3 == 0]
+            counter = 0
+            res[frame-1,:] = keypoints
+            break
+
+    return res
+
+
+def peakdet(v, delta, x = None):
+    """
+    Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    
+    Returns two arrays
+    
+    function [maxtab, mintab]=peakdet(v, delta, x)
+    %PEAKDET Detect peaks in a vector
+    %        [MAXTAB, MINTAB] = PEAKDET(V, DELTA) finds the local
+    %        maxima and minima ("peaks") in the vector V.
+    %        MAXTAB and MINTAB consists of two columns. Column 1
+    %        contains indices in V, and column 2 the found values.
+    %      
+    %        With [MAXTAB, MINTAB] = PEAKDET(V, DELTA, X) the indices
+    %        in MAXTAB and MINTAB are replaced with the corresponding
+    %        X-values.
+    %
+    %        A point is considered a maximum peak if it has the maximal
+    %        value, and was preceded (to the left) by a value lower by
+    %        DELTA.
+    
+    % Eli Billauer, 3.4.05 (Explicitly not copyrighted).
+    % This function is released to the public domain; Any use is allowed.
+    
+    """
+    maxtab = []
+    mintab = []
+       
+    if x is None:
+        x = np.arange(len(v))
+    
+    v = np.asarray(v)
+    
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+    
+    if not np.isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+    
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+    
+    mn, mx = np.Inf, -np.Inf
+    mnpos, mxpos = np.NaN, np.NaN
+    
+    lookformax = True
+    
+    for i in np.arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+        
+        if lookformax:
+            if this < mx-delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn+delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    return np.array(maxtab), np.array(mintab)
+
+
+def get_angle(A,B,C,data):
+    """
+    finds the angle ABC, assumes that confidence columns have been removed
+    A,B and C are integers corresponding to different keypoints
+    """
+    p_A = np.array([data[:,3*A],data[:,3*A+1]]).T
+    p_B = np.array([data[:,3*B],data[:,3*B+1]]).T
+    p_C = np.array([data[:,3*C],data[:,3*C+1]]).T
+    p_BA = p_A - p_B
+    p_BC = p_C - p_B
+    dot_products = np.sum(p_BA*p_BC,axis=1)
+    norm_products = np.linalg.norm(p_BA,axis=1)*np.linalg.norm(p_BC,axis=1)
+    return np.arccos(dot_products/norm_products)
